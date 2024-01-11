404fb10d12d2dba9ff0bcef6b5214982b1003f33
Manuel <manuel@guerrerocabrera.com>
2024-01-11 12:32:56 +0100

    Insertando imágen logo

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const divApp = document.querySelector("#app");
+
+const imgLogo = document.createElement("img");
+imgLogo.src = "/assets/img/logo.png";
+
+divApp.append(imgLogo);
