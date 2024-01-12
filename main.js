cc1972bb67228c11086d2dcc2488767777d226ed
Manuel <manuel@guerrerocabrera.com>
2024-01-12 13:20:32 +0100

    Añadiendo menú de navegación

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Header } from './src/components/Header/Header';
+import { HorizontalList } from './src/components/HorizontalList/HorizontalList';
 import './style.css';
 
 const divApp = document.querySelector("#app");
@@ -6,5 +7,12 @@ const divApp = document.querySelector("#app");
 // Crear elemento Header
 const header = Header();
 
+// Crear elemento HorizontalList
+const navMenu = HorizontalList();
+// Añadir al header el namMenu
+header.appendChild(navMenu);
+
 // Pintar el header
 divApp.appendChild(header);
+
+
--- src/components/HorizontalList/HorizontalList.js
+++ src/components/HorizontalList/HorizontalList.js
@@ -1,1 +1,63 @@
 import "./HorizontalList.css";
+
+const optionsMenu = [
+    {
+        url: '#',
+        textcontent: 'Sobre mí',
+        title: 'Sobre mí'
+    },
+    {
+        url: '#',
+        textcontent: 'Experiencia',
+        title: 'Experiencia'
+    },
+    {
+        url: '#',
+        textcontent: 'Formación',
+        title: 'Formación'
+    },
+    {
+        url: '#',
+        textcontent: 'Portfolio',
+        title: 'Portfolio'
+    }
+];
+
+export const HorizontalList = () => {
+
+    // Crear elemento ul
+    const ul = document.createElement("ul");
+    // Incluir el id nav-menu al elemento
+    ul.id = "nav-menu";
+    // Obtener valores del array de objetos optionsMenu creando un li con una a para cada elemento y añadirlo a la lista    
+    const aUrl = optionsMenu.map((a) => {
+        return a.url;
+    });
+    const aTextContent = optionsMenu.map((a) => {
+        return a.textcontent;
+    });
+    const aTitle = optionsMenu.map((a) => {
+        return a.title;
+    });
+
+    // Crear los elementos li e incluir los elementos a
+    for(let i = 0; i < optionsMenu.length; i++){
+        
+        // Crear elemento li
+        const li = document.createElement("li");
+        // Crear elemento a
+        const aHTML = document.createElement("a");
+        aHTML.href = aUrl[i];
+        aHTML.title = aTitle[i];
+        aHTML.textContent = aTextContent[i];
+        // Inyectar elemento a en el li
+        li.appendChild(aHTML);
+        // Inyectar el elemento li al ul
+        ul.appendChild(li);
+
+    }
+    
+    // Devolver el ul con los li
+    return ul;
+
+}
